ce5ed7b4df805822a56d52c51ed4fde702d54e2b
Isaac Perez <97491749+AlejandroIPerezL@users.noreply.github.com>
2026-07-15 21:49:36 -0500

    Delete build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug directory
    Por subir repetido

--- build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Proyecto-Programacion_FILE D:/Documentos/Proyecto-Programacion/build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/Proyecto-Programacion.exe)
-set(__QT_DEPLOY_TARGET_Proyecto-Programacion_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Proyecto-Programacion_RUNTIME_DLLS D:/Qt/6.11.1/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.11.1/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.11.1/mingw_64/bin/Qt6Core.dll)
